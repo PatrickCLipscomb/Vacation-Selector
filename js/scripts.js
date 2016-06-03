6ea4836543c4364518f2879c0f5774541b172b55
Patrick Lipscomb <patrick@>
2016-06-03 09:27:14 -0700

    add logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,16 @@
 $(function() {
-  $('form#personalbtn').submit(function(event) {
+  $('form#blanks').submit(function(event) {
     event.preventDefault();
     $('#home').hide();
     $('#personal').show();
 
   });
+
+  $('form#boxes').submit(function(event) {
+    event.preventDefault();
+    $('#personal').hide();
+    var selection = 
+    if ()
+  }
 
 });
